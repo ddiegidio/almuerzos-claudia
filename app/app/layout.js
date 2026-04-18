c81903ce468a0fb1abe7f7ea6eb3007fe00c2017
ddiegidio <ddiegidio@gmail.com>
2026-04-18 01:35:14 -0400

    Delete app/app directory

--- app/app/layout.js
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({ children }) {
-  return (
-    <html lang="es">
-      <body style={{ margin: 0, fontFamily: 'Arial, sans-serif' }}>
-        {children}
-      </body>
-    </html>
-  )
-}
